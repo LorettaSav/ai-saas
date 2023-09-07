3aa525114ae1f5663fd6e2b6f0765780f036c922
LorettaSav <loretta.sav.6@gmail.com>
2023-09-08 00:44:40 +0300

    complete mobile-sidebar

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,12 +1,23 @@
-"use client"
+"use client";
 
 import { Button } from "./ui/button";
 import { Menu } from "lucide-react";
+import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
+import Sidebar from "./sidebar";
 
 export default function MobileSidebar() {
   return (
-    <Button variant="ghost" size="icon" className="md:hidden">
-      <Menu />
-    </Button>
+    <>
+      <Sheet>
+        <SheetTrigger>
+          <Button variant="ghost" size="icon" className="md:hidden">
+            <Menu />
+          </Button>
+              </SheetTrigger>
+              <SheetContent side="left" className="p-0">
+                  <Sidebar/>
+              </SheetContent>
+      </Sheet>
+    </>
   );
 }
